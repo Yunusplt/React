a64cd8d644059371d87035ae10e4f1d42674bea7
yunus <yunuspolat2528@gmail.com>
2023-06-07 14:06:16 +0200

    tailwind

---
+++ class-notes/DE-20-TailwindCss-Kurulum/src/App.js
@@ -0,0 +1,51 @@
+function App() {
+  return (
+    <div
+      className="bg-fuchsia-300 h-screen flex justify-center items-center
+    gap-3"
+    >
+      {/* <h1 className="text-3xl font-bold underline">Hello</h1> */}
+      {/* <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 p-4">
+        <h2>Title</h2>
+        <img
+          src="https://cdn.pixabay.com/photo/2023/05/27/11/12/naxos-8021321_1280.jpg"
+          className="h-[250px] rounded-xl"
+          alt=""
+        />
+        <p className="text-white line-clamp-2 hover:line-clamp-none">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus,
+          recusandae similique dolore labore deserunt cumque voluptate omnis
+          odio, explicabo debitis autem laborum? Tenetur, quibusdam qui.
+        </p>
+      </div> */}
+      <div className="flex flex-col justify-between items-center bg-slate-500 rounded-xl w-3/12 p-4">
+        <h2>Title</h2>
+        <img
+          src="https://cdn.pixabay.com/photo/2023/05/27/11/12/naxos-8021321_1280.jpg"
+          className="h-[250px] rounded-xl"
+          alt=""
+        />
+        <p className="text-white line-clamp-2 hover:line-clamp-none">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus,
+          recusandae similique dolore labore deserunt cumque voluptate omnis
+          odio, explicabo debitis autem laborum? Tenetur, quibusdam qui.
+        </p>
+      </div>
+      <div className="card">
+        <h2>Title</h2>
+        <img
+          src="https://cdn.pixabay.com/photo/2023/05/27/11/12/naxos-8021321_1280.jpg"
+          className="h-[250px] rounded-xl"
+          alt=""
+        />
+        <p className="text-white line-clamp-2 hover:line-clamp-none">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus,
+          recusandae similique dolore labore deserunt cumque voluptate omnis
+          odio, explicabo debitis autem laborum? Tenetur, quibusdam qui.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
