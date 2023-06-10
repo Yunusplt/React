4c55878710dea8e49af159f348794fcc53a8d219
yunus <yunuspolat2528@gmail.com>
2023-06-10 14:34:23 +0200

    movie app in class deleted

--- class-notes/DE-21-22-movie-app/movie-app-in-class/src/App.js
+++ class-notes/DE-21-22-movie-app/movie-app-in-class/src/App.js
@@ -1,17 +1,10 @@
 import React from "react";
 import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
-import AppRouter from "./router/AppRouter";
-import AuthContextProvider from "./context/AuthContext";
-import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <div>
-    <AuthContextProvider name="felix">
-      <AppRouter/>
-      <ToastContainer/>
-    </AuthContextProvider>
+      <Register />
     </div>
   );
 };
