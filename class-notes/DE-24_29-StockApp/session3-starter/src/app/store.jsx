caef5a65c4e819eaf6109fe3a6582da1eca296fd
yunus <yunuspolat2528@gmail.com>
2023-06-19 12:51:54 +0200

    Stock app 3. ders sonu

--- class-notes/DE-24_29-StockApp/session3-starter/src/app/store.jsx
+++ class-notes/DE-24_29-StockApp/session3-starter/src/app/store.jsx
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
-import stockReducer from "../features/stockSlice";
 import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 // localStorage.setItem("user", user)
 
@@ -15,7 +14,6 @@ const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: {
     auth: persistedReducer,
-    stock: stockReducer
   },
   devTools: process.env.NODE_ENV !== "production",
 });
